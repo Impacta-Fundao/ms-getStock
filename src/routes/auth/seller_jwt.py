b1f6a515af91eeb9045e6370961526fb2a58bcfc
Pedro Rogel <pedrorogel3@gmail.com>
2025-09-02 15:12:41 -0300

    feat: implement jwt service

--- src/routes/auth/seller_jwt.py
+++ src/routes/auth/seller_jwt.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import jwt_required, create_access_token
+from flask_jwt_extended import create_access_token
+from src.Application.Service.seller_service import ...
 
 seller_jwt_bp = Blueprint('seller_jwt', __name__)
 
