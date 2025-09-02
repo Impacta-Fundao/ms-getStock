d013f05ff8d44c4d6d6587a9016b5d613bd1de5a
Pedro Rogel <pedrorogel3@gmail.com>
2025-09-02 16:58:57 -0300

    feat: create login route to do the authentication

--- src/routes/auth/seller_jwt.py
+++ src/routes/auth/seller_jwt.py
@@ -1,14 +1,23 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import create_access_token
-from src.Application.Service.seller_service import ...
+from src.Application.Service.seller_service import SellerService 
 
 seller_jwt_bp = Blueprint('seller_jwt', __name__)
 
 @seller_jwt_bp.route('/login', methods=['POST'])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
-    if not username == 'murillo' and not password == 'admin1234':
-        return jsonify({"msg": "Bad username or password"}), 401
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
+    try:    
+        username = request.json.get("email", None)
+        password = request.json.get("senha", None)
+        seller = SellerService.authenticate(username,password)
+        
+        if seller:
+            access_token = create_access_token(identity=str(seller.id))
+            return jsonify({
+                "access_token":access_token,
+                "message":"Login realizado com sucesso",
+                "seller_id": seller.id,
+                "nome": seller.nome
+                }),200
+    except:
+        return jsonify({"message": "Usuário não autorizado - Email ou senha incorretos"})
