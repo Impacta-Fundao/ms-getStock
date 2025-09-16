9fc90c8824b0c1405176ac51102bb739517d2040
Pedro Rogel <pedrorogel3@gmail.com>
2025-09-16 09:36:05 -0300

    feat: add CORS to allow application access

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@ from src.config_db.db import Config_db,Config_db_hml,Config_db_prd
 from config import  Config
 from flask_jwt_extended import JWTManager
 from src.Infrastructure.auth.seller_jwt import jwt_service
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -14,6 +15,8 @@ def create_app(config_class=Config_db,):
     app.config["JWT_SECRET_KEY"] = Config_db.JWT_SECRET_KEY
     db.init_app(app)
     jwt_service.init_app(app)
+    CORS(app)
+
 
     from src.routes.auth.seller_jwt import seller_jwt_bp
     from src.routes.sellers.route import init_route
