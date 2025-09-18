7fc942f621442703f847c8f2cdf55f1a67352884
Gustavo Oliveira <gustavorodriguesoliveira2013@gmail.com>
2025-09-18 16:32:12 -0300

    refactor: refactor jwt login logic

--- src/routes/auth/seller_jwt.py
+++ src/routes/auth/seller_jwt.py
@@ -5,19 +5,19 @@ from src.Application.Service.seller_service import SellerService
 seller_jwt_bp = Blueprint('seller_jwt', __name__)
 
 @seller_jwt_bp.route('/login', methods=['POST'])
-def login():
-    try:    
-        username = request.json.get("email", None)
-        password = request.json.get("senha", None)
-        seller = SellerService.authenticate(username,password)
-        
-        if seller:
-            access_token = create_access_token(identity=str(seller.id))
-            return jsonify({
-                "access_token":access_token,
-                "message":"Login realizado com sucesso",
-                "seller_id": seller.id,
-                "nome": seller.nome
-                }),200
-    except:
-        return jsonify({"message": "Usuário não autorizado - Email ou senha incorretos"})
+def login():    
+    username = request.json.get("email", None)
+    password = request.json.get("senha", None)
+
+    seller = SellerService.authenticate(username,password)
+    
+    if seller:
+        access_token = create_access_token(identity=str(seller.id))
+        return jsonify({
+            "access_token":access_token,
+            "message":"Login realizado com sucesso",
+            "seller_id": seller.id,
+            "nome": seller.nome
+            }),200
+    else:
+        return jsonify({"message": "Usuário não autorizado - Email ou senha incorretos"}), 401
